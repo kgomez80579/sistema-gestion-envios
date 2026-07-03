52f70ee8f5a931154ff85f6d5e2fe300156ecf72
Emily <leivavegaemily@gmail.com>
2026-07-03 00:04:59 -0600

    01:Conexion a GitHub (Inicializacion)

---
+++ src/main/java/com/sistemaGestionEnvios/SistemasGestionEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaGestionEnvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasGestionEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasGestionEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaGestionEnvios/SistemasGestionEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaGestionEnvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasGestionEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
